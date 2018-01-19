c8829b020058b4eaee3e4d9198e93cb2950375bb
Hong Wang Choi <hongwangchoi@Hongs-MacBook-Pro.local>
2018-01-19 12:28:39 +0900

    gui init

--- src/game/GameController.java
+++ src/game/GameController.java
@@ -5,13 +5,17 @@ import javafx.scene.control.Label;
 
 public class GameController 
 {
-    @FXML
-    private Label ScoreLabel;
-    @FXML
-    private Label HealthLabel;
-    
-    public GameController() {
-    }
-    
+	@FXML
+	private Label ScoreLabel;
+	@FXML
+	private Label HealthLabel;
+
+	public GameController() {
+	}
+	public void setText()
+	{
+		ScoreLabel.setText("Score: 0");
+		HealthLabel.setText("Health: 100");
+	}
 
 }
--- src/game/Main.java
+++ src/game/Main.java
@@ -3,51 +3,69 @@ package game;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
+
+import java.io.IOException;
+
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.StackPane;
 import javafx.util.Duration;
 
 public class Main extends Application {
-    private Stage primaryStage;
-    private StackPane GamePane;
-    
-  @Override // Override the start method in the Application class
-  public void start(Stage primaryStage) {
-	  this.primaryStage=primaryStage;
-	  
-	  /*
+	private Stage primaryStage;
+	private StackPane gamePane;
+
+	@Override // Override the start method in the Application class
+	public void start(Stage primaryStage) {
+		try {
+			this.primaryStage=primaryStage;
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(Main.class.getResource("Game.fxml"));
+			
+			gamePane=(StackPane) loader.load();
+			GameController gameController=loader.getController();
+			gameController.setText();
+			Scene scene=new Scene(gamePane);
+			primaryStage.setScene(scene);
+			primaryStage.setTitle("game");
+			primaryStage.show(); 
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		/*
     ClockPane clock = new ClockPane(); // Create a clock
 
     // Create a handler for animation
     EventHandler<ActionEvent> eventHandler = e -> {
       clock.setCurrentTime(); // Set a new clock time
     };
-    
+
     // Create an animation for a running clock
     Timeline animation = new Timeline(
       new KeyFrame(Duration.millis(1000), eventHandler));
     animation.setCycleCount(Timeline.INDEFINITE);
     animation.play(); // Start animation
-    
+
     // Create a scene and place it in the stage
     Scene scene = new Scene(clock, 250, 250);
-    */
-    primaryStage.setTitle("ClockAnimation"); // Set the stage title
-    //primaryStage.setScene(scene); // Place the scene in the stage
-    //primaryStage.setResizable(false);
-    primaryStage.show(); // Display the stage
-  }
+		 */
+ // Set the stage title
+		//primaryStage.setScene(scene); // Place the scene in the stage
+		//primaryStage.setResizable(false);
+// Display the stage
+	}
 
-  /**
-   * The main method is only needed for the IDE with limited
-   * JavaFX support. Not needed for running from the command line.
-   */
-  public static void main(String[] args) {
-    launch(args);
-  }
+	/**
+	 * The main method is only needed for the IDE with limited
+	 * JavaFX support. Not needed for running from the command line.
+	 */
+	public static void main(String[] args) {
+		launch(args);
+	}
 }
 
