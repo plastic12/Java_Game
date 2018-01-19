24ddceec60855ed2264303df42931e55fb47bd40
Hong Wang Choi <hongwangchoi@Hongs-MacBook-Pro.local>
2018-01-19 12:08:34 +0900

    gui init

---
+++ src/game/GameController.java
@@ -0,0 +1,17 @@
+package game;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class GameController 
+{
+    @FXML
+    private Label ScoreLabel;
+    @FXML
+    private Label HealthLabel;
+    
+    public GameController() {
+    }
+    
+
+}
