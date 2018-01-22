32f01d22b37c5dacf12a32c5a19dfcda29083cb7
Yan233456 <35454296+Yan233456@users.noreply.github.com>
2018-01-22 20:27:32 +0800

    upload start

---
+++ src/game/view/StartController.java
@@ -0,0 +1,31 @@
+package game.view;
+
+import java.io.IOException;
+
+import game.Main;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
+
+public class StartController {
+
+    @FXML
+    private Label Leaderboard;
+
+    @FXML
+    private Label startBtn;
+
+    @FXML
+    private Label GameName;
+
+    @FXML
+    private Pane StartPane;
+
+    @FXML
+    public void startGame(MouseEvent event) throws IOException {
+    	Main.Game();
+    }
+    
+}
