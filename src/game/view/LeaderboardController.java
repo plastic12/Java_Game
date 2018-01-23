63e34acffc818f425f3da2015a078b5da4d68399
Yan233456 <35454296+Yan233456@users.noreply.github.com>
2018-01-23 16:25:35 +0800

    Add files via upload

---
+++ src/game/view/LeaderboardController.java
@@ -0,0 +1,34 @@
+package game.view;
+
+import java.io.IOException;
+
+import game.Main;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+
+public class LeaderboardController {
+
+    @FXML
+    private Label thirdScore;
+
+    @FXML
+    private Label firstScore;
+
+    @FXML
+    private Label menu;
+
+    @FXML
+    private Label secondScore;
+
+    @FXML
+    void backMenu(MouseEvent event) throws IOException {
+    	Main.menu();
+    }
+
+    void init() {
+    	//read file
+    	//sort
+    	//change label
+    }
+}
