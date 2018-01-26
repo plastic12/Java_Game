9028ae5f6340f539dcf7c9c3ad082f1987ad5df8
Yan233456 <35454296+Yan233456@users.noreply.github.com>
2018-01-26 19:43:13 +0800

    Update Sound.java

--- src/game/Sound.java
+++ src/game/Sound.java
@@ -7,14 +7,15 @@ import javafx.scene.media.MediaPlayer;
 public class Sound 
 {
 	private Media music;
-	private MediaPlayer playerl
+	private MediaPlayer player;
 		
 	public void set() {
 		try {
-			music = new Media(ClassLoader.getSystemResouce("liquid.wav").toURI().toString());
+			music = new Media(ClassLoader.getSystemResource("liquid.wav").toURI().toString());
 		} catch (URISyntaxException e) {
 			System.out.println("Media not found");
 		}
 		MediaPlayer player = new MediaPlayer(music);
 		player.play();
+	}
 }
