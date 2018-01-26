74466b1e84ebd1b594571c8e47e15498001a9e53
Yan233456 <35454296+Yan233456@users.noreply.github.com>
2018-01-26 17:15:55 +0800

    Update Main.java

--- src/game/Main.java
+++ src/game/Main.java
@@ -77,14 +77,14 @@ public class Main extends Application{
 		if(controller==null)
 			System.out.println("fail");
 		controller.init(score);
+		LeaderboardController.addScore(score);
 		Scene scene = new Scene(pane);
 		primaryStage.setScene(scene);
 	}
 	
 	public static void viewLeaderboard() throws IOException {
 		FXMLLoader loader1 = new FXMLLoader();
-		loader1.setLocation(Main.class.getResource("view/Leaderboard.fxml"));	
-		
+		loader1.setLocation(Main.class.getResource("view/Leaderboard.fxml"));			
 		Pane leader = (Pane) loader1.load();
 
 		Scene leaderScene = new Scene(leader);
